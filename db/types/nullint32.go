0e3ff57640cafdc5b2bf6ca08906dc09a2d1dedc
PYTNAG <sgds.2020.nag@yandex.ru>
2023-10-24 12:20:32 +0500

    Implement Valuer interface

--- db/types/nullint32.go
+++ db/types/nullint32.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"database/sql/driver"
 	"encoding/json"
 )
 
@@ -11,6 +12,12 @@ func (i NullInt32) MarshalJSON() ([]byte, error) {
 	if i.Valid {
 		return json.Marshal(i.Int32)
 	}
-
 	return json.Marshal(nil)
+}
+
+func (i NullInt32) Value() (driver.Value, error) {
+	if !i.Valid {
+		return nil, nil
+	}
+	return int64(i.Int32), nil
 }
